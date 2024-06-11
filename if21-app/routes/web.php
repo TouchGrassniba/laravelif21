4f2b00314da950e2c64bd660c97420b8a61e0dda
NuelBlasius <161033492+NuelBlasius@users.noreply.github.com>
2024-06-11 18:42:35 +0700

    update GACHOR

--- if21-app/routes/web.php
+++ if21-app/routes/web.php
@@ -1,23 +1,28 @@
 <?php
-
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FakultasController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\ProdiController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('about', function () {
-    return "Halaman about";
+Route::resource('fakultas', FakultasController::class)->middleware(['auth', 'verified']);
+Route::resource('prodi', ProdiController::class)->middleware(['auth', 'verified']);
+Route::resource('mahasiswa', MahasiswaController::class)->middleware(['auth', 'verified']);
+Route::get('dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('profile', function () {
-    return view('profile');
-});
-Route::resource('fakultas', FakultasController::class);
-Route::resource('prodi', ProdiController::class);
-Route::resource('mahasiswa', MahasiswaController::class);
-Route::get('dashboard', [DashboardController::class, 'index']);
+require __DIR__ . '/auth.php';
