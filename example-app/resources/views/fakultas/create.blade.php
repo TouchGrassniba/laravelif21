ebd410f1695830b9dc50d06f85ad516f74eefe43
NuelBlasius <161033492+NuelBlasius@users.noreply.github.com>
2024-06-21 19:35:16 +0700

    update

--- example-app/resources/views/fakultas/create.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layout.main')
-
-@section('title', 'Tambah Fakultas')
-
-@section('content')
-    <h2>Daftar Fakultas</h2>
-    <p>ini halaman tambah fakultas</p>
-@endsection
--- example-app/resources/views/prodi/create.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layout.main')
-
-@section('title', 'Tambah Program Studi')
-
-@section('content')
-    <h2>Daftar Program Studi</h2>
-    <p>ini halaman program studi</p>
-@endsection
